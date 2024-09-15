2f4a319b686a2ede88b031e5e4e8577cfafbb3b9
rbm0524 <rbm0524@naver.com>
2024-09-15 20:21:35 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/ordertogether/team14_be/Team14BeApplication.java
@@ -0,0 +1,13 @@
+package com.ordertogether.team14_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team14BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team14BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ordertogether/team14_be/Team14BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordertogether.team14_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team14BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
