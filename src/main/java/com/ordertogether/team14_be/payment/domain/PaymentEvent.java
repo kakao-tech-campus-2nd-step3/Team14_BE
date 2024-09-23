3543ee7a1cc5aa0eb069d1222eac2031c8cfdba0
나제법 <skwpqjq@gmail.com>
2024-09-24 00:26:23 +0900

    style: spotless 적용
    # Conflicts:
    #	src/main/java/com/ordertogether/team14_be/memebr/persistence/entity/Member.java
    #	src/main/java/com/ordertogether/team14_be/payment/domain/PaymentEvent.java
    #	src/main/java/com/ordertogether/team14_be/payment/domain/PaymentOrder.java
    #	src/main/java/com/ordertogether/team14_be/payment/domain/PaymentOrderStatus.java
    #	src/main/java/com/ordertogether/team14_be/payment/domain/Product.java
    #	src/main/java/com/ordertogether/team14_be/spot/controller/SpotController.java
    #	src/main/java/com/ordertogether/team14_be/spot/dto/SpotDto.java
    #	src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
    #	src/main/java/com/ordertogether/team14_be/spot/repository/SpotRepository.java
    #	src/main/java/com/ordertogether/team14_be/spot/service/SpotService.java

--- src/main/java/com/ordertogether/team14_be/payment/domain/PaymentEvent.java
+++ src/main/java/com/ordertogether/team14_be/payment/domain/PaymentEvent.java
@@ -1,34 +1,41 @@
 package com.ordertogether.team14_be.payment.domain;
 
-import java.math.BigDecimal;
-import java.util.List;
+import com.ordertogether.team14_be.common.persistence.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
-@Builder
+@Entity
 @Getter
+@SuperBuilder
 @ToString
-public class PaymentEvent {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentEvent extends BaseTimeEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private Long buyerId;
+	@Column(nullable = false)
+	private Long buyerId; // 구매자 식별자
 
-	private List<PaymentOrder> paymentOrders;
-
+	@Column(nullable = false)
 	private String orderId;
 
 	private String orderName;
 
-	private String paymentKey;
+	@Column(nullable = false)
+	private String paymentKey; // PSP 결제 식별자
 
-	@Builder.Default private PaymentStatus paymentStatus = PaymentStatus.READY;
-
-	public Long totalAmount() {
-		return paymentOrders.stream()
-				.map(PaymentOrder::getAmount)
-				.reduce(BigDecimal.ZERO, BigDecimal::add)
-				.longValue();
-	}
+	@Builder.Default private Boolean isPaymentDone = false;
 }
--- src/main/java/com/ordertogether/team14_be/payment/domain/Product.java
+++ src/main/java/com/ordertogether/team14_be/payment/domain/Product.java
@@ -1,18 +1,34 @@
 package com.ordertogether.team14_be.payment.domain;
 
+import com.ordertogether.team14_be.common.persistence.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
-@Builder
+@Entity
 @Getter
+@SuperBuilder
 @ToString
-public class Product {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseTimeEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String name;
 
+	@Column(nullable = false)
 	private BigDecimal price;
 }
