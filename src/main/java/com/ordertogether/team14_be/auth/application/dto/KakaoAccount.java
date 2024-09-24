401536de4eee57b9bdc0698c01391673e8c54afa
westzeroright <kt8267@naver.com>
2024-09-25 04:40:29 +0900

    refactor: 불필요한 어노테이션 제거
    Json 필드 이름과 KakaoAccount 객체의 필드 이름이 같음

--- src/main/java/com/ordertogether/team14_be/auth/application/dto/KakaoAccount.java
+++ src/main/java/com/ordertogether/team14_be/auth/application/dto/KakaoAccount.java
@@ -1,5 +1,3 @@
 package com.ordertogether.team14_be.auth.application.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record KakaoAccount(@JsonProperty("email") String email) {}
+public record KakaoAccount(String email) {}
