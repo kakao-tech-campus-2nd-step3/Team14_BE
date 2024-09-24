d58f2e75a437e6a605731e994d819505f55988d2
westzeroright <kt8267@naver.com>
2024-09-25 04:43:52 +0900

    refactor: @RequiredArgsConstructor 적용
    생성자 생략 가능

--- src/main/java/com/ordertogether/team14_be/memebr/application/service/MemberService.java
+++ src/main/java/com/ordertogether/team14_be/memebr/application/service/MemberService.java
@@ -2,16 +2,14 @@ package com.ordertogether.team14_be.memebr.application.service;
 
 import com.ordertogether.team14_be.memebr.persistence.MemberRepository;
 import com.ordertogether.team14_be.memebr.persistence.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
 	private final MemberRepository memberRepository;
-
-	public MemberService(MemberRepository memberRepository) {
-		this.memberRepository = memberRepository;
-	}
 
 	public void findOrCreateMember(String email) {
 		Member member =
