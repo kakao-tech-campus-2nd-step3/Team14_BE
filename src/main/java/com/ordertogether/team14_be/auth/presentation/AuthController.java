11e44e601863668d4e504af392d5e2f11a0070e5
westzeroright <kt8267@naver.com>
2024-09-25 04:43:52 +0900

    refactor: @RequiredArgsConstructor 적용
    생성자 생략 가능

--- src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
+++ src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
@@ -1,20 +1,18 @@
 package com.ordertogether.team14_be.auth.presentation;
 
 import com.ordertogether.team14_be.auth.application.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
 	private final AuthService authService;
-
-	public AuthController(AuthService authService) {
-		this.authService = authService;
-	}
 
 	@GetMapping("/login")
 	public String getToken(@RequestHeader String authorizationCode) {
