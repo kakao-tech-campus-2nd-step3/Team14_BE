2f08714cb884c68806177f2fb02b05181544b8af
BOMIN LYU <rbm0524@naver.com>
2024-09-25 21:35:28 +0900

    feat: Soft delete를 위해 isDeleted 속성 추가

--- src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
+++ src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
@@ -1,36 +1,42 @@
 package com.ordertogether.team14_be.spot.entity;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Table(indexes = {@Index(name = "idx_lat_lng", columnList = "lat, lng")})
 public class Spot {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(precision = 10, scale = 8)
-    private BigDecimal lat;
+  @Column(precision = 10, scale = 8)
+  private BigDecimal lat;
 
-    @Column(precision = 11, scale = 8)
-    private BigDecimal lng;
-    private String category;
-    private String store_name;
-    private Integer minimum_order_amount;
+  @Column(precision = 11, scale = 8)
+  private BigDecimal lng;
 
-    @Lob
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String together_order_link;
-    private String pick_up_location;
-    private String delivery_status;
+  private String category;
+  private String store_name;
+  private Integer minimum_order_amount;
 
+  @Lob
+  @Column(columnDefinition = "MEDIUMTEXT")
+  private String together_order_link;
+
+  private String pick_up_location;
+  private String delivery_status;
+  @Builder.Default private Boolean isDeleted = false;
+
+  public void setDeleted(boolean isDeleted) {
+    this.isDeleted = isDeleted;
+  }
 }
