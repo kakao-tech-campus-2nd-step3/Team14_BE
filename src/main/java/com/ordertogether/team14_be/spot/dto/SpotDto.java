f93760e77ed11956bd6014e2617f4e771e212db2
BOMIN LYU <rbm0524@naver.com>
2024-09-25 21:42:32 +0900

    refactor: isDeleted 추가

--- src/main/java/com/ordertogether/team14_be/spot/dto/SpotDto.java
+++ src/main/java/com/ordertogether/team14_be/spot/dto/SpotDto.java
@@ -13,32 +13,34 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class SpotDto {
-	private Long id;
+  private Long id;
 
-	@Column(precision = 10, scale = 8)
-	private BigDecimal lat;
+  @Column(precision = 10, scale = 8)
+  private BigDecimal lat;
 
-	@Column(precision = 11, scale = 8)
-	private BigDecimal lng;
+  @Column(precision = 11, scale = 8)
+  private BigDecimal lng;
 
-	private String category;
-	private String store_name;
-	private int minimum_order_amount;
-	private String together_order_link;
-	private String pick_up_location;
-	private String delivery_status;
+  private String category;
+  private String store_name;
+  private int minimum_order_amount;
+  private String together_order_link;
+  private String pick_up_location;
+  private String delivery_status;
+  private boolean isDeleted;
 
-	public Spot toEntity() {
-		return Spot.builder()
-				.id(id)
-				.lat(lat)
-				.lng(lng)
-				.category(category)
-				.store_name(store_name)
-				.minimum_order_amount(minimum_order_amount)
-				.together_order_link(together_order_link)
-				.pick_up_location(pick_up_location)
-				.delivery_status(delivery_status)
-				.build();
-	}
+  public Spot toEntity() {
+    return Spot.builder()
+        .id(id)
+        .lat(lat)
+        .lng(lng)
+        .category(category)
+        .store_name(store_name)
+        .minimum_order_amount(minimum_order_amount)
+        .together_order_link(together_order_link)
+        .pick_up_location(pick_up_location)
+        .delivery_status(delivery_status)
+        .isDeleted(isDeleted)
+        .build();
+  }
 }
