72eb28e9dfd29b849e3478a93007cb8ec0633cb5
BOMIN LYU <rbm0524@naver.com>
2024-09-26 15:12:00 +0900

    refactor: category의 타입을 Category(ENUM 클래스)로 변경

--- src/main/java/com/ordertogether/team14_be/spot/dto/SpotDto.java
+++ src/main/java/com/ordertogether/team14_be/spot/dto/SpotDto.java
@@ -1,11 +1,10 @@
 package com.ordertogether.team14_be.spot.dto;
 
 import com.ordertogether.team14_be.spot.entity.Spot;
+import com.ordertogether.team14_be.spot.enums.Category;
 import jakarta.persistence.Column;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
 import lombok.*;
 
 @Builder
@@ -13,38 +12,38 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class SpotDto {
-    private Long id;
+  private Long id;
 
-    @Column(precision = 10, scale = 8)
-    private BigDecimal lat;
+  @Column(precision = 10, scale = 8)
+  private BigDecimal lat;
 
-    @Column(precision = 11, scale = 8)
-    private BigDecimal lng;
+  @Column(precision = 11, scale = 8)
+  private BigDecimal lng;
 
-    private String category;
-    private String store_name;
-    private int minimum_order_amount;
-    private String together_order_link;
-    private String pick_up_location;
-    private String delivery_status;
-    private boolean isDeleted;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+  private Category category;
+  private String store_name;
+  private int minimum_order_amount;
+  private String together_order_link;
+  private String pick_up_location;
+  private String delivery_status;
+  private boolean isDeleted;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
-    public Spot toEntity() {
-        return Spot.builder()
-                .id(id)
-                .lat(lat)
-                .lng(lng)
-                .category(category)
-                .store_name(store_name)
-                .minimum_order_amount(minimum_order_amount)
-                .together_order_link(together_order_link)
-                .pick_up_location(pick_up_location)
-                .delivery_status(delivery_status)
-                .isDeleted(isDeleted)
-                .createdAt(createdAt)
-                .updatedAt(updatedAt)
-                .build();
-    }
+  public Spot toEntity() {
+    return Spot.builder()
+        .id(id)
+        .lat(lat)
+        .lng(lng)
+        .category(category)
+        .store_name(store_name)
+        .minimum_order_amount(minimum_order_amount)
+        .together_order_link(together_order_link)
+        .pick_up_location(pick_up_location)
+        .delivery_status(delivery_status)
+        .isDeleted(isDeleted)
+        .createdAt(createdAt)
+        .updatedAt(updatedAt)
+        .build();
+  }
 }
