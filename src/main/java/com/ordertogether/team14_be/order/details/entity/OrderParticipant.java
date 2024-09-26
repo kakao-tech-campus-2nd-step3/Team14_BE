07f2f305d3ff0288849bd656eae1d32751eee00e
ajy9851 <ajy4304@naver.com>
2024-09-26 16:25:12 +0900

    fix: OrderParticipant에 어노테이션 추가

--- src/main/java/com/ordertogether/team14_be/order/details/entity/OrderParticipant.java
+++ src/main/java/com/ordertogether/team14_be/order/details/entity/OrderParticipant.java
@@ -9,10 +9,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 // 주문 정보에는 참여자 정보(즉 여러 명의 Member)가 필요합니다.
 // 따라서 Member 와 다대다 연결을 위한 중간 테이블(연결 엔티티)를 만들었습니다.
 @Entity
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "order_participant")
 public class OrderParticipant {
 
