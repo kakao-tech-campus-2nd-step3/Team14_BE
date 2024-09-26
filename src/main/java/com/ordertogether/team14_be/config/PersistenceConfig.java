b996d58a4800bc67b7a31e41722f164eb7043edc
나제법 <skwpqjq@gmail.com>
2024-09-27 02:18:34 +0900

    feat: 결제 Repository 빈 등록

--- src/main/java/com/ordertogether/team14_be/config/PersistenceConfig.java
+++ src/main/java/com/ordertogether/team14_be/config/PersistenceConfig.java
@@ -1,6 +1,15 @@
 package com.ordertogether.team14_be.config;
 
 import com.ordertogether.team14_be.common.persistence.auditing.AuditorProvider;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.JpaPaymentEventRepository;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.JpaPaymentOrderRepository;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.JpaProductRepository;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.SimpleJpaPaymentEventRepository;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.SimpleJpaPaymentOrderRepository;
+import com.ordertogether.team14_be.payment.persistence.jpa.repository.SimpleJpaProductRepository;
+import com.ordertogether.team14_be.payment.persistence.repository.PaymentEventRepository;
+import com.ordertogether.team14_be.payment.persistence.repository.PaymentOrderRepository;
+import com.ordertogether.team14_be.payment.persistence.repository.ProductRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -14,5 +23,25 @@ public class PersistenceConfig {
 	@Bean
 	public AuditorAware<Long> auditorProvider() {
 		return new AuditorProvider();
+	}
+
+	@Bean
+	public PaymentEventRepository paymentEventRepository(
+			SimpleJpaPaymentEventRepository simpleJpaPaymentEventRepository) {
+		return new JpaPaymentEventRepository(simpleJpaPaymentEventRepository);
+	}
+
+	@Bean
+	public PaymentOrderRepository paymentOrderRepository(
+			SimpleJpaPaymentOrderRepository simpleJpaPaymentOrderRepository,
+			SimpleJpaProductRepository simpleJpaProductRepository) {
+		return new JpaPaymentOrderRepository(
+				simpleJpaPaymentOrderRepository, simpleJpaProductRepository);
+	}
+
+	@Bean
+	public ProductRepository productRepository(
+			SimpleJpaProductRepository simpleJpaProductRepository) {
+		return new JpaProductRepository(simpleJpaProductRepository);
 	}
 }
