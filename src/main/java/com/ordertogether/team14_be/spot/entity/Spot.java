6336feb2c7fa7547fd81c2a88e713059862e2dce
rbm0524 <rbm0524@naver.com>
2024-09-26 21:36:24 +0900

    refactor: setter로 변경하던 isDeleted를 delete와 restore 메서드로 변경

--- src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
+++ src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
@@ -1,11 +1,12 @@
 package com.ordertogether.team14_be.spot.entity;
 
-import com.ordertogether.team14_be.payment.domain.BaseEntity;
+import com.ordertogether.team14_be.common.persistence.entity.BaseEntity;
 import com.ordertogether.team14_be.spot.enums.Category;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @SuperBuilder // 상속받은 필드도 빌더에서 사용
@@ -13,27 +14,36 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(indexes = {@Index(name = "idx_lat_lng", columnList = "lat, lng")})
+@DynamicUpdate // 변경한 필드만 대응
 public class Spot extends BaseEntity {
 
-  @Column(precision = 10, scale = 8)
-  private BigDecimal lat;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-  @Column(precision = 11, scale = 8)
-  private BigDecimal lng;
+	@Column(precision = 10, scale = 8)
+	private BigDecimal lat;
 
-  private Category category;
-  private String store_name;
-  private Integer minimum_order_amount;
+	@Column(precision = 11, scale = 8)
+	private BigDecimal lng;
 
-  @Lob
-  @Column(columnDefinition = "MEDIUMTEXT")
-  private String together_order_link;
+	private Category category;
+	private String storeName;
+	private Integer minimumOrderAmount;
 
-  private String pick_up_location;
-  private String delivery_status;
-  @Builder.Default private Boolean isDeleted = false;
+	@Lob
+	@Column(columnDefinition = "MEDIUMTEXT")
+	private String togetherOrderLink;
 
-  public void setDeleted(boolean isDeleted) {
-    this.isDeleted = isDeleted;
-  }
+	private String pickUpLocation;
+	private String deliveryStatus;
+	@Builder.Default private Boolean isDeleted = false;
+
+	public void delete() {
+		this.isDeleted = true;
+	}
+
+	public void restore() {
+		this.isDeleted = false;
+	}
 }
