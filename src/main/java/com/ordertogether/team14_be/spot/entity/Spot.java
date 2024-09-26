34c44c1fe6ae03f76c86106cf61f781b765ead6a
BOMIN LYU <rbm0524@naver.com>
2024-09-26 15:12:00 +0900

    refactor: category의 타입을 Category(ENUM 클래스)로 변경

--- src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
+++ src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
@@ -1,10 +1,9 @@
 package com.ordertogether.team14_be.spot.entity;
 
 import com.ordertogether.team14_be.payment.domain.BaseEntity;
+import com.ordertogether.team14_be.spot.enums.Category;
 import jakarta.persistence.*;
-
 import java.math.BigDecimal;
-
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -16,26 +15,25 @@ import lombok.experimental.SuperBuilder;
 @Table(indexes = {@Index(name = "idx_lat_lng", columnList = "lat, lng")})
 public class Spot extends BaseEntity {
 
-    @Column(precision = 10, scale = 8)
-    private BigDecimal lat;
+  @Column(precision = 10, scale = 8)
+  private BigDecimal lat;
 
-    @Column(precision = 11, scale = 8)
-    private BigDecimal lng;
+  @Column(precision = 11, scale = 8)
+  private BigDecimal lng;
 
-    private String category;
-    private String store_name;
-    private Integer minimum_order_amount;
+  private Category category;
+  private String store_name;
+  private Integer minimum_order_amount;
 
-    @Lob
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String together_order_link;
+  @Lob
+  @Column(columnDefinition = "MEDIUMTEXT")
+  private String together_order_link;
 
-    private String pick_up_location;
-    private String delivery_status;
-    @Builder.Default
-    private Boolean isDeleted = false;
+  private String pick_up_location;
+  private String delivery_status;
+  @Builder.Default private Boolean isDeleted = false;
 
-    public void setDeleted(boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
+  public void setDeleted(boolean isDeleted) {
+    this.isDeleted = isDeleted;
+  }
 }
