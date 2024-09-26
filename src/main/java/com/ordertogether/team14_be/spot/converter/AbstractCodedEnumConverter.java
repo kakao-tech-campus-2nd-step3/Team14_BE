5e805d96378d06b892648db34909bd71c5f6de54
rbm0524 <rbm0524@naver.com>
2024-09-26 21:37:12 +0900

    fix: Converter 어노테이션 붙임

--- src/main/java/com/ordertogether/team14_be/spot/converter/AbstractCodedEnumConverter.java
+++ src/main/java/com/ordertogether/team14_be/spot/converter/AbstractCodedEnumConverter.java
@@ -1,9 +1,12 @@
 package com.ordertogether.team14_be.spot.converter;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
 import java.util.Arrays;
 import java.util.Objects;
 
+@Converter
 public class AbstractCodedEnumConverter<T extends Enum<T> & CodedEnum<E>, E>
 		implements AttributeConverter<T, E> {
 
