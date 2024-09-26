62e7c7f9b4939ce4abbdae9610e2cddf6f7f5986
나제법 <skwpqjq@gmail.com>
2024-09-27 01:28:59 +0900

    feat: Domain 구현

--- src/main/java/com/ordertogether/team14_be/payment/domain/PaymentEvent.java
+++ src/main/java/com/ordertogether/team14_be/payment/domain/PaymentEvent.java
@@ -1,41 +1,34 @@
 package com.ordertogether.team14_be.payment.domain;
 
-import com.ordertogether.team14_be.common.persistence.entity.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
-@Entity
+@Builder
 @Getter
-@SuperBuilder
 @ToString
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PaymentEvent extends BaseTimeEntity {
+public class PaymentEvent {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
-	private Long buyerId; // 구매자 식별자
+	private Long buyerId;
 
-	@Column(nullable = false)
+	private List<PaymentOrder> paymentOrders;
+
 	private String orderId;
 
 	private String orderName;
 
-	@Column(nullable = false)
-	private String paymentKey; // PSP 결제 식별자
+	private String paymentKey;
 
-	@Builder.Default private Boolean isPaymentDone = false;
+	@Builder.Default private PaymentStatus paymentStatus = PaymentStatus.READY;
+
+	public Long totalAmount() {
+		return paymentOrders.stream()
+				.map(PaymentOrder::getAmount)
+				.reduce(BigDecimal.ZERO, BigDecimal::add)
+				.longValue();
+	}
 }
