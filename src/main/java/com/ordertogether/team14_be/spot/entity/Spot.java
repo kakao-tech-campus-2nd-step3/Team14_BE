5b815d693025a2580083128c8349d3e5e6292f40
BOMIN LYU <rbm0524@naver.com>
2024-09-26 12:53:52 +0900

    refactor: BaseEntity 상속하도록 수정

--- src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
+++ src/main/java/com/ordertogether/team14_be/spot/entity/Spot.java
@@ -1,42 +1,41 @@
 package com.ordertogether.team14_be.spot.entity;
 
+import com.ordertogether.team14_be.payment.domain.BaseEntity;
 import jakarta.persistence.*;
+
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-@Builder
-@AllArgsConstructor
+@SuperBuilder // 상속받은 필드도 빌더에서 사용
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(indexes = {@Index(name = "idx_lat_lng", columnList = "lat, lng")})
-public class Spot {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+public class Spot extends BaseEntity {
 
-  @Column(precision = 10, scale = 8)
-  private BigDecimal lat;
+    @Column(precision = 10, scale = 8)
+    private BigDecimal lat;
 
-  @Column(precision = 11, scale = 8)
-  private BigDecimal lng;
+    @Column(precision = 11, scale = 8)
+    private BigDecimal lng;
 
-  private String category;
-  private String store_name;
-  private Integer minimum_order_amount;
+    private String category;
+    private String store_name;
+    private Integer minimum_order_amount;
 
-  @Lob
-  @Column(columnDefinition = "MEDIUMTEXT")
-  private String together_order_link;
+    @Lob
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String together_order_link;
 
-  private String pick_up_location;
-  private String delivery_status;
-  @Builder.Default private Boolean isDeleted = false;
+    private String pick_up_location;
+    private String delivery_status;
+    @Builder.Default
+    private Boolean isDeleted = false;
 
-  public void setDeleted(boolean isDeleted) {
-    this.isDeleted = isDeleted;
-  }
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
