5cb076b3b3dbbf414d8b3b7a65fcc2d11e4298a3
ajy9851 <ajy4304@naver.com>
2024-09-27 22:06:42 +0900

    feat: OrderDetailServcie 생성
    주문 상세 정보 생성 메서드만 작성
    추후 RUD도 제작 예정

---
+++ src/main/java/com/ordertogether/team14_be/order/details/service/OrderDetailService.java
@@ -0,0 +1,65 @@
+package com.ordertogether.team14_be.order.details.service;
+
+import com.ordertogether.team14_be.memebr.persistence.MemberRepository;
+import com.ordertogether.team14_be.memebr.persistence.entity.Member;
+import com.ordertogether.team14_be.order.details.dto.create.CreateOrderDetailRequestDto;
+import com.ordertogether.team14_be.order.details.dto.create.CreateOrderDetailResponseDto;
+import com.ordertogether.team14_be.order.details.entity.OrderDetail;
+import com.ordertogether.team14_be.order.details.repository.OrderDetailRepository;
+import com.ordertogether.team14_be.order.details.repository.OrderParticipantRepository;
+import com.ordertogether.team14_be.spot.entity.Spot;
+import com.ordertogether.team14_be.spot.repository.SpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderDetailService {
+
+	private final OrderDetailRepository orderDetailRepository;
+	private final OrderParticipantRepository orderParticipantRepository;
+	private final MemberRepository MemberRepository;
+	private final SpotRepository spotRepository;
+
+	public OrderDetailService(
+			OrderDetailRepository orderDetailRepository,
+			OrderParticipantRepository orderParticipantRepository,
+			com.ordertogether.team14_be.memebr.persistence.MemberRepository memberRepository,
+			SpotRepository spotRepository) {
+		this.orderDetailRepository = orderDetailRepository;
+		this.orderParticipantRepository = orderParticipantRepository;
+		MemberRepository = memberRepository;
+		this.spotRepository = spotRepository;
+	}
+
+	// 주문 상세 정보 생성 메서드
+	public CreateOrderDetailResponseDto createOrderDetail(
+			CreateOrderDetailRequestDto createOrderDetailRequestDto) {
+		// 방장 정보 설정
+		Member leader =
+				MemberRepository.findById(createOrderDetailRequestDto.getLeaderId())
+						.orElseThrow(() -> new IllegalArgumentException("방장 정보가 없습니다."));
+
+		// 스팟 정보 설정
+		Spot spot =
+				spotRepository
+						.findById(createOrderDetailRequestDto.getSpotId())
+						.orElseThrow(() -> new IllegalArgumentException("스팟 정보를 찾을 수 없습니다."));
+
+		OrderDetail orderDetail =
+				OrderDetail.builder()
+						.leader(leader)
+						.spot(spot)
+						.price(createOrderDetailRequestDto.getPrice())
+						.isPayed(createOrderDetailRequestDto.isPayed())
+						.build();
+
+		OrderDetail savedOrderDetail = orderDetailRepository.save(orderDetail);
+
+		return CreateOrderDetailResponseDto.builder()
+				.id(savedOrderDetail.getId())
+				.price(savedOrderDetail.getPrice())
+				.isPayed(savedOrderDetail.isPayed())
+				.leaderName(leader.getDeliveryName())
+				.spotName(spot.getStore_name())
+				.build();
+	}
+}
