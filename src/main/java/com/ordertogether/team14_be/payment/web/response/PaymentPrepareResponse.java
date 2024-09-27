cdab4b18743218b6cb2d0849ed84a77819271bac
나제법 <skwpqjq@gmail.com>
2024-09-27 20:07:06 +0900

    feat: DTO record로 변경

--- src/main/java/com/ordertogether/team14_be/payment/web/response/PaymentPrepareResponse.java
+++ src/main/java/com/ordertogether/team14_be/payment/web/response/PaymentPrepareResponse.java
@@ -3,31 +3,22 @@ package com.ordertogether.team14_be.payment.web.response;
 import com.ordertogether.team14_be.payment.domain.PaymentEvent;
 import com.ordertogether.team14_be.payment.domain.PaymentOrder;
 import java.util.List;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@Builder
-@ToString
-public class PaymentPrepareResponse {
-
-	private Long paymentEventId;
-	private Long buyerId;
-	private List<PaymentOrderResponse> paymentOrders;
-	private String orderId;
-	private String orderName;
-	private String paymentKey;
-
+public record PaymentPrepareResponse(
+		Long paymentEventId,
+		Long buyerId,
+		List<PaymentOrderResponse> paymentOrders,
+		String orderId,
+		String orderName,
+		String paymentKey) {
 	public static PaymentPrepareResponse of(
 			PaymentEvent paymentEvent, List<PaymentOrder> paymentOrders) {
-		return PaymentPrepareResponse.builder()
-				.paymentEventId(paymentEvent.getId())
-				.buyerId(paymentEvent.getBuyerId())
-				.paymentOrders(paymentOrders.stream().map(PaymentOrderResponse::from).toList())
-				.orderId(paymentEvent.getOrderId())
-				.orderName(paymentEvent.getOrderName())
-				.paymentKey(paymentEvent.getPaymentKey())
-				.build();
+		return new PaymentPrepareResponse(
+				paymentEvent.getId(),
+				paymentEvent.getBuyerId(),
+				paymentOrders.stream().map(PaymentOrderResponse::from).toList(),
+				paymentEvent.getOrderId(),
+				paymentEvent.getOrderName(),
+				paymentEvent.getPaymentKey());
 	}
 }
