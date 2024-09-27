21a1546b0ed4535c9aa2f60dd46f39bca7329e83
나제법 <skwpqjq@gmail.com>
2024-09-27 20:07:06 +0900

    feat: DTO record로 변경

--- src/main/java/com/ordertogether/team14_be/payment/web/response/PaymentOrderResponse.java
+++ src/main/java/com/ordertogether/team14_be/payment/web/response/PaymentOrderResponse.java
@@ -1,32 +1,15 @@
 package com.ordertogether.team14_be.payment.web.response;
 
 import com.ordertogether.team14_be.payment.domain.PaymentOrder;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@Builder
-@ToString
-public class PaymentOrderResponse {
-
-	private final Long paymentOrderId;
-
-	private final Long productId;
-
-	private final String orderId;
-
-	private final String orderName;
-
-	private final Long amount;
-
+public record PaymentOrderResponse(
+		Long paymentOrderId, Long productId, String orderId, String orderName, Long amount) {
 	public static PaymentOrderResponse from(PaymentOrder paymentOrder) {
-		return PaymentOrderResponse.builder()
-				.paymentOrderId(paymentOrder.getId())
-				.productId(paymentOrder.getProductId())
-				.orderId(paymentOrder.getOrderId())
-				.orderName(paymentOrder.getOrderName())
-				.amount(paymentOrder.getAmount().longValue())
-				.build();
+		return new PaymentOrderResponse(
+				paymentOrder.getId(),
+				paymentOrder.getProductId(),
+				paymentOrder.getOrderId(),
+				paymentOrder.getOrderName(),
+				paymentOrder.getAmount().longValue());
 	}
 }
