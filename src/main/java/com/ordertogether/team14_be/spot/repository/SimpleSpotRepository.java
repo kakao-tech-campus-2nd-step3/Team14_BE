43d5f46277b3c30e664dad1d9e0fd41982f3b65c
BOMIN LYU <rbm0524@naver.com>
2024-10-03 22:45:33 +0900

    refactor: 이름 수정

--- src/main/java/com/ordertogether/team14_be/spot/repository/SimpleSpotRepository.java
+++ src/main/java/com/ordertogether/team14_be/spot/repository/SimpleSpotRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpotRepository extends JpaRepository<Spot, Long> {
+public interface SimpleSpotRepository extends JpaRepository<Spot, Long> {
 	List<Spot> findByLatAndLngAndIsDeletedFalse(BigDecimal lat, BigDecimal lng);
 
 	Optional<Spot> findByIdAndIsDeletedFalse(Long id);
