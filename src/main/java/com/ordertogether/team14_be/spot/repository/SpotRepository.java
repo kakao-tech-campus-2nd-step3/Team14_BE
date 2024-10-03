970565341b0cbf9f94215f612487f614efc2dc41
BOMIN LYU <rbm0524@naver.com>
2024-10-03 22:50:18 +0900

    feat: SimpleSpotRepository를 가지는 Repository 계층 생성

---
+++ src/main/java/com/ordertogether/team14_be/spot/repository/SpotRepository.java
@@ -0,0 +1,42 @@
+package com.ordertogether.team14_be.spot.repository;
+
+import com.ordertogether.team14_be.spot.dto.servicedto.SpotDto;
+import com.ordertogether.team14_be.spot.entity.Spot;
+import com.ordertogether.team14_be.spot.mapper.SpotMapper;
+import jakarta.persistence.EntityNotFoundException;
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class SpotRepository {
+
+	private final SimpleSpotRepository simpleSpotRepository;
+
+	public SpotDto save(Spot spot) {
+		return SpotMapper.INSTANCE.toDto(simpleSpotRepository.save(spot));
+	}
+
+	public SpotDto findByIdAndIsDeletedFalse(Long id) {
+		return SpotMapper.INSTANCE.toDto(
+				simpleSpotRepository
+						.findByIdAndIsDeletedFalse(id)
+						.orElseThrow(() -> new EntityNotFoundException(id + "에 해당하는 Spot을 찾을 수 없습니다.")));
+	}
+
+	public List<SpotDto> findByLatAndLngAndIsDeletedFalse(BigDecimal lat, BigDecimal lng) {
+		return simpleSpotRepository.findByLatAndLngAndIsDeletedFalse(lat, lng).stream()
+				.map(SpotMapper.INSTANCE::toDto)
+				.toList();
+	}
+
+	public void delete(Long id) {
+		Spot spot =
+				simpleSpotRepository
+						.findByIdAndIsDeletedFalse(id)
+						.orElseThrow(() -> new EntityNotFoundException(id + "에 해당하는 Spot을 찾을 수 없습니다."));
+		spot.delete();
+	}
+}
