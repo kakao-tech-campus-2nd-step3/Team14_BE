7163afec007b6e9fd2944c01e78f11856f20084f
BOMIN LYU <rbm0524@naver.com>
2024-10-11 21:35:56 +0900

    feat: 반경 n미터 내 Spot들만 처리해서 반환하는 메서드 작성

--- src/main/java/com/ordertogether/team14_be/spot/service/SpotService.java
+++ src/main/java/com/ordertogether/team14_be/spot/service/SpotService.java
@@ -1,5 +1,7 @@
 package com.ordertogether.team14_be.spot.service;
 
+import static java.lang.Math.abs;
+
 import com.ordertogether.team14_be.spot.dto.controllerdto.SpotCreationResponse;
 import com.ordertogether.team14_be.spot.dto.controllerdto.SpotDetailResponse;
 import com.ordertogether.team14_be.spot.dto.controllerdto.SpotViewedResponse;
@@ -16,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class SpotService {
-
+	public static final int EARTH_RADIUS = 6371000; // 6371km
 	private final SpotRepository spotRepository;
 
 	// Spot 전체 조회하기
@@ -38,6 +40,44 @@ public class SpotService {
 	public SpotDetailResponse getSpot(Long id) {
 		SpotDto spotDto = spotRepository.findByIdAndIsDeletedFalse(id);
 		return SpotMapper.INSTANCE.toSpotDetailResponse(spotDto);
+	}
+
+	// 반경 n미터 내 Spot 조회하기
+	@Transactional(readOnly = true)
+	public List<SpotViewedResponse> getSpotByRadius(BigDecimal lat, BigDecimal lng, int radius) {
+		// m당 y 좌표 이동 값
+		double mForLatitude = (1 / (EARTH_RADIUS * 1 * (Math.PI / 180))) / 1000;
+		// m당 x 좌표 이동 값
+		double mForLongitude =
+				(1 / (EARTH_RADIUS * 1 * (Math.PI / 180) * Math.cos(Math.toRadians(lat.doubleValue()))))
+						/ 1000;
+
+		// 현재 위치 기준 검색 거리 좌표
+		double maxY = lat.doubleValue() + (radius * mForLatitude);
+		double minY = lat.doubleValue() - (radius * mForLatitude);
+		double maxX = lng.doubleValue() + (radius * mForLongitude);
+		double minX = lng.doubleValue() - (radius * mForLongitude);
+
+		// 원의 지름에 해당하는 정사각형 내에 있는 Spot들을 모두 가져옴
+		List<SpotDto> resultAroundSpot =
+				spotRepository.findAroundSpotAndIsDeletedFalse(
+						BigDecimal.valueOf(maxX),
+						BigDecimal.valueOf(maxY),
+						BigDecimal.valueOf(minX),
+						BigDecimal.valueOf(minY));
+
+		// 자기 위치에서부터 반경 내에 있는 Spot만 반환
+		return resultAroundSpot.stream()
+				.filter(
+						spotDto -> {
+							double distance =
+									Math.sqrt(
+											Math.pow(abs(spotDto.getLat().doubleValue() - lat.doubleValue()), 2)
+													+ Math.pow(abs(spotDto.getLng().doubleValue() - lng.doubleValue()), 2));
+							return distance <= radius;
+						})
+				.map(SpotMapper.INSTANCE::toSpotViewedResponse)
+				.toList();
 	}
 
 	@Transactional
