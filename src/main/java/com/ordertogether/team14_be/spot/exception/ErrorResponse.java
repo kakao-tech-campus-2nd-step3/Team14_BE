b99a0c4baae8ad41042edb0936e3a0df3acbe90d
westzeroright <kt8267@naver.com>
2024-10-12 18:03:15 +0900

    fix: 프로그램 수행 가능하도록 함

--- src/main/java/com/ordertogether/team14_be/spot/exception/ErrorResponse.java
+++ src/main/java/com/ordertogether/team14_be/spot/exception/ErrorResponse.java
@@ -1,7 +1,6 @@
 package com.ordertogether.team14_be.spot.exception;
 
 import com.ordertogether.team14_be.spot.enums.ErrorCode;
-import lombok.Getter;
 
-@Getter
+// @Getter
 public record ErrorResponse(String message, ErrorCode code) {}
