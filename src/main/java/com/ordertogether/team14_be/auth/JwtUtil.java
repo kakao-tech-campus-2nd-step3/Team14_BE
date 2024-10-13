610badbe1c8e2e7876219fdec02936c156b11047
westzeroright <kt8267@naver.com>
2024-10-14 01:03:26 +0900

    feat: 토큰 해독 메서드 구현

--- src/main/java/com/ordertogether/team14_be/auth/JwtUtil.java
+++ src/main/java/com/ordertogether/team14_be/auth/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.ordertogether.team14_be.auth;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -8,15 +9,18 @@ import java.time.Duration;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtUtil {
 	private final SecretKey key;
+	private final int expireTime;
 
-	@Value("${jwt.expire-time}")
-	private int expireTime;
-
-	public JwtUtil(@Value("${key.jwt.secret-key}") String secretKey) {
+	public JwtUtil(
+			@Value("${key.jwt.secret-key}") String secretKey,
+			@Value("${jwt.expire-time}") int expireTime) {
 		this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+		this.expireTime = expireTime;
 	}
 
 	public String generateToken(Long data) {
@@ -29,5 +33,9 @@ public class JwtUtil {
 				.setExpiration(exp)
 				.signWith(key, SignatureAlgorithm.HS256)
 				.compact();
+	}
+
+	public Claims decodeJwt(String token) {
+		return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
 	}
 }
