a0b17a447e22ac35613ef058176c8fa640fb8482
westzeroright <kt8267@naver.com>
2024-10-16 01:25:46 +0900

    refactor: 클라이언트는 멤버아이디를 모른다.. 토큰 사용..

--- src/main/java/com/ordertogether/team14_be/member/presentation/MemberController.java
+++ src/main/java/com/ordertogether/team14_be/member/presentation/MemberController.java
@@ -4,12 +4,12 @@ import com.ordertogether.team14_be.common.web.response.ApiResponse;
 import com.ordertogether.team14_be.member.application.dto.MemberInfoRequest;
 import com.ordertogether.team14_be.member.application.dto.MemberInfoResponse;
 import com.ordertogether.team14_be.member.application.service.MemberService;
+import com.ordertogether.team14_be.member.persistence.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,25 +22,24 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@GetMapping("/{memberId}")
-	public ResponseEntity<ApiResponse<MemberInfoResponse>> getMemberInfo(
-			@PathVariable Long memberId) {
-		MemberInfoResponse data = memberService.findMemberInfo(memberId);
+	@GetMapping
+	public ResponseEntity<ApiResponse<MemberInfoResponse>> getMemberInfo(@LoginMember Member member) {
+		MemberInfoResponse data = memberService.findMemberInfo(member.getId());
 		return ResponseEntity.ok(ApiResponse.with(HttpStatus.OK, "회원 정보를 가져옵니다.", data));
 	}
 
-	@PutMapping("/{memberId}")
+	@PutMapping
 	public ResponseEntity<ApiResponse<MemberInfoResponse>> modifyMemberInfo(
-			@PathVariable Long memberId, @RequestBody MemberInfoRequest memberInfoRequest) {
+			@LoginMember Member member, @RequestBody MemberInfoRequest memberInfoRequest) {
 		MemberInfoResponse data =
 				memberService.modifyMember(
-						memberId, memberInfoRequest.deliveryName(), memberInfoRequest.phoneNumber());
+						member.getId(), memberInfoRequest.deliveryName(), memberInfoRequest.phoneNumber());
 		return ResponseEntity.ok(ApiResponse.with(HttpStatus.OK, "회원 정보가 수정되었습니다.", data));
 	}
 
-	@DeleteMapping("/{memberId}") // 회원 탈퇴
-	public ResponseEntity<ApiResponse> deleteMember(@PathVariable Long memberId) {
-		memberService.deleteMember(memberId);
+	@DeleteMapping
+	public ResponseEntity<ApiResponse> deleteMember(@LoginMember Member member) {
+		memberService.deleteMember(member.getId());
 		return ResponseEntity.ok(ApiResponse.with(HttpStatus.OK, "회원 정보가 삭제되었습니다.", ""));
 	}
 }
