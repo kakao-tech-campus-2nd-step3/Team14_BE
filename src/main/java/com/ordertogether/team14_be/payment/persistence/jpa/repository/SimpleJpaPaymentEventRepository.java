4fa74ed26e3df466816c898d9c42b521fdea1930
나제법 <skwpqjq@gmail.com>
2024-11-01 09:47:09 +0900

    feat: PaymentEventRepository 결제 상태 변경 기능 구현

--- src/main/java/com/ordertogether/team14_be/payment/persistence/jpa/repository/SimpleJpaPaymentEventRepository.java
+++ src/main/java/com/ordertogether/team14_be/payment/persistence/jpa/repository/SimpleJpaPaymentEventRepository.java
@@ -1,12 +1,30 @@
 package com.ordertogether.team14_be.payment.persistence.jpa.repository;
 
+import com.ordertogether.team14_be.payment.domain.PaymentStatus;
 import com.ordertogether.team14_be.payment.persistence.jpa.entity.PaymentEventEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SimpleJpaPaymentEventRepository extends JpaRepository<PaymentEventEntity, Long> {
 
 	Optional<PaymentEventEntity> findByOrderId(String orderId);
+
+	@Modifying
+	@Query(
+			"UPDATE PaymentEventEntity pee"
+					+ " SET pee.paymentStatus = :afterStatus"
+					+ " WHERE pee.paymentKey = :paymentKey"
+					+ "  AND pee.orderId = :orderId")
+	Integer updatePaymentStatus(String paymentKey, String orderId, PaymentStatus afterStatus);
+
+	@Modifying
+	@Query(
+			"UPDATE PaymentEventEntity pee"
+					+ " SET pee.paymentKey = :paymentKey"
+					+ " WHERE pee.orderId = :orderId")
+	Integer updatePaymentKey(String paymentKey, String orderId);
 }
