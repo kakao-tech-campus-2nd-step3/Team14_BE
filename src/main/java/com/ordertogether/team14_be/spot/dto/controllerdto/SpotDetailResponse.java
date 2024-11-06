c6d9ab081f33114924df7b4af63186dfa9211908
BOMIN LYU <rbm0524@naver.com>
2024-11-06 19:24:27 +0900

    refactor: String으로 타입 변환

--- src/main/java/com/ordertogether/team14_be/spot/dto/controllerdto/SpotDetailResponse.java
+++ src/main/java/com/ordertogether/team14_be/spot/dto/controllerdto/SpotDetailResponse.java
@@ -1,9 +1,7 @@
 package com.ordertogether.team14_be.spot.dto.controllerdto;
 
-import com.ordertogether.team14_be.spot.enums.Category;
-
 public record SpotDetailResponse(
-		Category category,
+		String category,
 		String storeName,
 		Integer minimumOrderAmount,
 		String pickUpLocation,
