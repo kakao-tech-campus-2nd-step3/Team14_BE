87224b18cc1015859a3088652adbfdc526fb5adb
BOMIN LYU <rbm0524@naver.com>
2024-11-06 19:21:24 +0900

    refactor: code와 ENUM값 변환 수정

--- src/main/java/com/ordertogether/team14_be/spot/converter/AbstractCodedEnumConverter.java
+++ src/main/java/com/ordertogether/team14_be/spot/converter/AbstractCodedEnumConverter.java
@@ -17,13 +17,14 @@ public class AbstractCodedEnumConverter<T extends Enum<T> & CodedEnum<E>, E>
 
 	@SuppressWarnings("unchecked") // 경고 억제
 	@Override
+	// Entity의 enum값을 DB에 변환하는 방식을 정의
 	public E convertToDatabaseColumn(
 			T attribute) { // Converts the value stored in the entity attribute into the data
 		// representation to be stored in the database.
-		// return attribute.getCode(); -> 코드 저장 ex) KOREAN_STEW -> "찜, 탕, 찌개"
-		return (E) attribute.name(); // Enum의 이름을 그대로 반환 -> DB에 ENUM을 그대로 저장
+		return attribute.getCode(); // 코드 저장 ex) KOREAN_STEW -> "찜, 탕, 찌개"
 	}
 
+	// DB값을 Entity의 enum값으로 변환하는 방식을 정의
 	@Override
 	public T convertToEntityAttribute(
 			E dbData) { // Converts the data stored in the database column into the value to be stored
