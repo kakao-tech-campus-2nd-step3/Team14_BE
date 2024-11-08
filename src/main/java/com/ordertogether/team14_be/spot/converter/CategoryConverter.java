579045c07d54108b5b803b7487f3cebfaa2d684f
BOMIN LYU <rbm0524@naver.com>
2024-11-08 17:31:28 +0900

    style: 코드 스타일 수정

--- src/main/java/com/ordertogether/team14_be/spot/converter/CategoryConverter.java
+++ src/main/java/com/ordertogether/team14_be/spot/converter/CategoryConverter.java
@@ -1,12 +1,12 @@
 package com.ordertogether.team14_be.spot.converter;
 
-import jakarta.persistence.Converter;
 import com.ordertogether.team14_be.spot.enums.Category;
+import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
 public class CategoryConverter extends AbstractCodedEnumConverter<Category, String> {
 
-    public CategoryConverter() {
-        super(Category.class); // Category.class를 부모 클래스에 전달
-    }
+	public CategoryConverter() {
+		super(Category.class); // Category.class를 부모 클래스에 전달
+	}
 }
