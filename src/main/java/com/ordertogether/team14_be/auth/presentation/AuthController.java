e322e827464cc690f3cd4b4b283d554860bb219e
westzeroright <kt8267@naver.com>
2024-11-08 15:12:32 +0900

    refactor: 페이지 리다이렉트 외부 url이 가능하도록 함

--- src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
+++ src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
@@ -6,24 +6,23 @@ import com.ordertogether.team14_be.common.web.response.ApiResponse;
 import com.ordertogether.team14_be.member.application.dto.MemberInfoRequest;
 import com.ordertogether.team14_be.member.application.service.MemberService;
 import com.ordertogether.team14_be.member.persistence.entity.Member;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
@@ -44,8 +43,13 @@ public class AuthController {
 	}
 
 	@GetMapping("/login")
-	public ResponseEntity<ApiResponse<String>> getToken(@RequestHeader String authorizationCode) {
+	public ResponseEntity<ApiResponse<String>> getToken(
+			@RequestHeader("Authorization") String authorizationHeader,
+			HttpServletResponse httpServletResponse) {
+		String authorizationCode = authorizationHeader.replace("Bearer ", "");
+		System.out.println("인가코드:" + authorizationCode);
 		String userKakaoEmail = kakaoAuthService.getKakaoUserEmail(authorizationCode);
+		System.out.println("이메일:" + userKakaoEmail);
 		Optional<Member> existMember = memberService.findMemberByEmail(userKakaoEmail);
 		if (existMember.isPresent()) {
 			String serviceToken = authService.getServiceToken(userKakaoEmail);
@@ -65,10 +69,14 @@ public class AuthController {
 					.headers(headers)
 					.body(ApiResponse.with(HttpStatus.OK, "로그인 성공", serviceToken));
 		} else {
-			return ResponseEntity.status(HttpStatus.FOUND)
-					.location(
-							URI.create(redirectPage + URLEncoder.encode(userKakaoEmail, StandardCharsets.UTF_8)))
-					.build();
+			String redirectPage = "http://localhost:3000/signup";
+			String redirectUrl = redirectPage + "?email=" + userKakaoEmail;
+			try {
+				httpServletResponse.sendRedirect(redirectUrl);
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+			return ResponseEntity.ok().body(ApiResponse.with(HttpStatus.OK, "리다이렉트", redirectUrl));
 		}
 	}
 
